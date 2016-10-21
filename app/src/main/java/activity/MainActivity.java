953eee57ac477b5f63a9c2eb921362c3c1a8566b
hemant314 <hemant@designstring.com>
2016-10-21 16:08:11 +0530

    final changes

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package activity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,6 +11,7 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -26,11 +28,13 @@ import static android.R.attr.data;
  * Created by hemanth on 20/10/16.
  */
 
-public  class MainActivity extends AppCompatActivity {
+public  class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     NotificationAdapter notificationAdapter;
     RecyclerView recyclerView;
+    Button linVertical,linHorizontal,gVertical,gHorizontal,sVertical;
+    boolean isLinVert,isLinHor,isGridVer,isGridHor,isStagVer,isStagHor=true;
 
     //String data[] =  {"hemanth","Nak","Pammi","barath","hemanth","Nak","Pammi","barath","hemanth","Nak","Pammi","barath","hemanth","Nak","Pammi","barath","hemanth","Nak","Pammi","barath"};
 
@@ -40,65 +44,179 @@ public  class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Holder dat[] = new Holder[]{
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.image_one,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.image_one,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.image_one,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.image_one,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.back,"title"),
-                new Holder(R.drawable.image_two,"title"),
-                new Holder(R.drawable.back,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+                new Holder(R.drawable.cheetha,"title"),
+                new Holder(R.drawable.twitter,"title"),
+                new Holder(R.drawable.eagle,"title"),
+
+
+
 
         };
 
-         //initialize recyclerview
+        //initialize recyclerview
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         //fixed size of recyclerview layout
         recyclerView.setHasFixedSize(true);
 
-        //initialize linear layout manager horizontally
-        LinearLayoutManager linearHorizontal = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        recyclerView.setLayoutManager(linearHorizontal);
 
         //intialize linear layout manager vertically
-        LinearLayoutManager linearVertical = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        LinearLayoutManager linearVertical = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(linearVertical);
 
-        //here the second parameter is for number of rows.
-        //third parameter is for horizontal scroll.
-        //fourth paramter is boolean,when it set to false,layout from start to end
-        GridLayoutManager gridHorizontal = new GridLayoutManager(this,3,GridLayoutManager.HORIZONTAL,false);
-        recyclerView.setLayoutManager(gridHorizontal);
 
+        linHorizontal = (Button) findViewById(R.id.linearHorizontal);
+        linVertical = (Button) findViewById(R.id.linearVertical);
+        gVertical = (Button) findViewById(R.id.gridVertical);
+        gHorizontal = (Button) findViewById(R.id.gridHorizontal);
+        sVertical = (Button) findViewById(R.id.staggeredVertical);
 
-        //for vertical scroll
-        GridLayoutManager gridVertical = new GridLayoutManager(this,3,GridLayoutManager.VERTICAL,false);
-        recyclerView.setLayoutManager(gridVertical);
+        linHorizontal.setOnClickListener(this);
+        linVertical.setOnClickListener(this);
+        gVertical.setOnClickListener(this);
+        gHorizontal.setOnClickListener(this);
+        sVertical.setOnClickListener(this);
 
-
-        //here first param represents number of rows
-        //second param represents horizontal scroll
-        StaggeredGridLayoutManager staggeredGridHorizontal=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(staggeredGridHorizontal);
-
-
-
-        //second param represents vertical scroll
-        StaggeredGridLayoutManager staggeredGridVertical=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(staggeredGridVertical);
 
         notificationAdapter = new NotificationAdapter(MainActivity.this,recyclerView,dat);
         recyclerView.setAdapter(notificationAdapter);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.linearVertical:
+
+                //intialize linear layout manager vertically
+                LinearLayoutManager linearVertical = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.VERTICAL,false);
+                recyclerView.setLayoutManager(linearVertical);
+
+                linVertical.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                linVertical.setTextColor(Color.parseColor("#ffffff"));
+                linHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                gVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                gHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                sVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linHorizontal.setTextColor(Color.parseColor("#000000"));
+                gVertical.setTextColor(Color.parseColor("#000000"));
+                gHorizontal.setTextColor(Color.parseColor("#000000"));
+                sVertical.setTextColor(Color.parseColor("#000000"));
+
+                break;
+
+            case R.id.linearHorizontal:
+
+                //initialize linear layout manager horizontally
+                LinearLayoutManager linearHorizontal = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+                recyclerView.setLayoutManager(linearHorizontal);
+
+                linHorizontal.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                linHorizontal.setTextColor(Color.parseColor("#ffffff"));
+                linVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                gVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                gHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                sVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linVertical.setTextColor(Color.parseColor("#000000"));
+                gVertical.setTextColor(Color.parseColor("#000000"));
+                gHorizontal.setTextColor(Color.parseColor("#000000"));
+                sVertical.setTextColor(Color.parseColor("#000000"));
+
+
+                break;
+
+            case R.id.gridVertical:
+
+                //for vertical scroll
+                GridLayoutManager gridVertical = new GridLayoutManager(MainActivity.this,3,GridLayoutManager.VERTICAL,false);
+                recyclerView.setLayoutManager(gridVertical);
+
+
+                gVertical.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                gVertical.setTextColor(Color.parseColor("#ffffff"));
+                linVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                gHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                sVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linVertical.setTextColor(Color.parseColor("#000000"));
+                linHorizontal.setTextColor(Color.parseColor("#000000"));
+                gHorizontal.setTextColor(Color.parseColor("#000000"));
+                sVertical.setTextColor(Color.parseColor("#000000"));
+
+                break;
+
+            case R.id.gridHorizontal:
+
+                //here the second parameter is for number of rows.
+                //third parameter is for horizontal scroll.
+                //fourth paramter is boolean,when it set to false,layout from start to end
+                GridLayoutManager gridHorizontal = new GridLayoutManager(MainActivity.this,2,GridLayoutManager.HORIZONTAL,false);
+                recyclerView.setLayoutManager(gridHorizontal);
+
+
+                gHorizontal.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                gHorizontal.setTextColor(Color.parseColor("#ffffff"));
+                linVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                gVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                sVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linVertical.setTextColor(Color.parseColor("#000000"));
+                linHorizontal.setTextColor(Color.parseColor("#000000"));
+                gVertical.setTextColor(Color.parseColor("#000000"));
+                sVertical.setTextColor(Color.parseColor("#000000"));
+
+                break;
+
+            case R.id.staggeredVertical:
+
+                //second param represents vertical scroll
+                StaggeredGridLayoutManager staggeredGridVertical=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(staggeredGridVertical);
+
+
+                sVertical.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                sVertical.setTextColor(Color.parseColor("#ffffff"));
+                linVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                linHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                gVertical.setBackgroundColor(getResources().getColor(R.color.back));
+                gHorizontal.setBackgroundColor(getResources().getColor(R.color.back));
+                linVertical.setTextColor(Color.parseColor("#000000"));
+                linHorizontal.setTextColor(Color.parseColor("#000000"));
+                gVertical.setTextColor(Color.parseColor("#000000"));
+                gHorizontal.setTextColor(Color.parseColor("#000000"));
+
+                break;
+
+
+            default:
+                break;
+
+        }
+
+
 
     }
 
